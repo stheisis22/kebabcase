c7549430fa67744b3e626701ad8f4a63f7673301
stheisis22 <44663142+stheisis22@users.noreply.github.com>
2021-12-01 00:19:43 -0400

    Add files via upload

---
+++ KebabCase/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace KebabCase
+{
+    class Program
+    {
+    static void Main(string[] args)
+        {
+            Console.WriteLine("Escriba el texto a convertir \n");
+            string value = Console.ReadLine();
+      
+            // Reemplaza con DASH
+            value = Regex.Replace(value, @"[^0-9a-zA-Z]", "-");
+
+            // Elimina dashes consecutivos
+            value = Regex.Replace(value, @"[-]{2,}", "-");
+
+            // Remueve los dashes del final
+            value = Regex.Replace(value, @"-+$", string.Empty);
+
+            // Remueve dashes al inicio
+            if (value.StartsWith("-")) value = value.Substring(1);
+
+              //Lowercase 
+            value=value.ToLower(); 
+            Console.WriteLine("To Kebab Case: "+value);
+
+        }
+    }
+}
